df8e06024696a3cd99639b556df52c36e3e1d148
Masahiro MIzumura <53962817+masahiro96848@users.noreply.github.com>
2024-05-21 02:06:08 +0000

    ログインの成功と失敗のロジックを追加して、flashを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new', status: :unprocessable_entity
+    end
   end
+
+  def destroy; end
 end
