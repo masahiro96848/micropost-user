11c024711170861b85439b44ee390f30b87aa00c
Masahiro MIzumura <53962817+masahiro96848@users.noreply.github.com>
2024-05-21 02:39:21 +0000

    認証済みかどうかのメソッドを作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      rest_session # ログインの直前に必ずこれを書くこと
+      reset_session # ログインの直前に必ずこれを書くこと
       log_in user
       redirect_to user
     else
