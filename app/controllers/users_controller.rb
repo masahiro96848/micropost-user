72e9d4479667d894ee8c4d00103aa2741e30a859
Masahiro MIzumura <53962817+masahiro96848@users.noreply.github.com>
2024-05-31 03:06:19 +0000

    フレンドリーフォワーディング機能とテストコードを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
   def logged_in_user
     return if logged_in?
 
+    store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url, status: :see_other
   end
