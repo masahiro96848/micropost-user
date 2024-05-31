2baa0972dd5d6f91771784590826b919d2400898
Masahiro MIzumura <53962817+masahiro96848@users.noreply.github.com>
2024-05-31 02:21:10 +0000

    リファクタリング

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,9 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する
+    # 詳しくは https://techracho.bpsinc.jp/hachi8833/2023_06_02/130443 を参照
+    session[:session_token] = user.session_token
   end
 
   # 永続的セッションのためにユーザーをデータベースに記憶する
@@ -14,7 +17,8 @@ module SessionsHelper
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      @current_user = user if user && session[:session_token] == user.session_token
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -22,6 +26,11 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
